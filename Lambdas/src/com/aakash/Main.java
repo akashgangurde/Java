9e27736caa53ebf38eb801e72f9bded012728e3e
Akash Gangurde <aakashg710@gmail.com>
2021-03-23 23:57:19 +0530

    Add files via upload

---
+++ Lambdas/src/com/aakash/Main.java
@@ -0,0 +1,153 @@
+package com.aakash;
+
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+//        new Thread(()-> {
+//            System.out.println("Printing from the Runnable");
+//            System.out.println("Line 2");
+//            System.out.format("This is Line %d\n", 3);
+//        }).start();
+
+        Employee john = new Employee("John Doe", 30);
+        Employee tim = new Employee("Tim Buchalka", 21);
+        Employee jack = new Employee("Jack Hill", 41);
+        Employee snow = new Employee("Snow White", 22);
+
+        List<Employee> employees = new ArrayList<>();
+        employees.add(john);
+        employees.add(tim);
+        employees.add(jack);
+        employees.add(snow);
+
+        employees.forEach(employee -> {
+            System.out.println(employee.getName());
+            System.out.println(employee.getAge());
+        });
+
+//        for (Employee employee : employees) {
+//            System.out.println(employee.getName());
+//            System.out.println(employee.getAge());
+//        }
+
+//        System.out.println("*****************************");
+//
+//        Employee employee;
+//
+//        for (int i = 0; i < employees.size(); i++) {
+//            employee = employees.get(i);
+//            System.out.println(employee.getName());
+//            new Thread(() -> System.out.println(employee.getAge())).start();
+//        }
+
+
+////        Collections.sort(employees, new Comparator<Employee>() {
+////            @Override
+////            public int compare(Employee employee1, Employee employee2) {
+////                return employee1.getName().compareTo(employee2.getName());
+////            }
+////        });
+//
+//        Collections.sort(employees, (employee1, employee2) ->
+//                employee1.getName().compareTo(employee2.getName()));
+//
+//        for (Employee employee : employees) {
+//            System.out.println(employee.getName());
+//        }
+//
+////        String sillyString = doStringStuff(new UpperConcat() {
+////            @Override
+////            public String upperAndConcat(String s1, String s2) {
+////                return s1.toUpperCase() + s2.toUpperCase();
+////            }
+////        },
+////                employees.get(0).getName(), employees.get(1).getName());
+////                System.out.println(sillyString);
+//
+//        UpperConcat uc = (s1, s2) -> {
+//            String result = s1.toUpperCase() + s2.toUpperCase();
+//            return result;
+//        };
+//        String sillyString = doStringStuff(uc, employees.get(0).getName(), employees.get(1).getName());
+//        System.out.println(sillyString);
+//
+//        AnotherClass anotherClass = new AnotherClass();
+//        String s = anotherClass.doSomething();
+//        System.out.println(s);
+    }
+
+    public final static String doStringStuff(UpperConcat uc, String s1, String s2) {
+        return uc.upperAndConcat(s1, s2);
+    }
+}
+
+class Employee {
+    private String name;
+    private int age;
+
+    public Employee(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
+
+interface UpperConcat {
+    public String upperAndConcat(String s1, String s2);
+}
+
+class AnotherClass {
+    public String doSomething() {
+
+        int i = 0;
+
+            UpperConcat uc = (s1, s2) -> {
+                System.out.println("The lambda expressions class is: " + getClass().getSimpleName());
+                System.out.println("i in the lambda expressions = " + i);
+                String result = s1.toUpperCase() + s2.toUpperCase();
+                return result;
+            };
+
+            System.out.println("The AnotherClass class's name is: " + getClass().getSimpleName());
+            return Main.doStringStuff(uc, "String1", "String2");
+
+    }
+
+    public void printValue() {
+        int number = 25;
+
+        Runnable r = () -> {
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("The value is " + number);
+        };
+
+        new Thread(r).start();
+    }
+}
